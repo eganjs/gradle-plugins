0868869d25947176521292a48b4f5bd671530bd2
Joseph Egan <joseph.s.egan@gmail.com>
2019-06-20 00:02:39 +0100

    Create subproject

---
+++ gradle-simple-versioning-plugin/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
